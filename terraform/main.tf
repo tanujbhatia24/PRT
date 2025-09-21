7ec54dbcbcb3e6e3abc65823701007b661f7b33f
tanujbhatia24 <tanujbhatia0001@gmail.com>
2025-09-21 18:11:18 +0530

    Update repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,39 @@
+data "aws_availability_zones" "available" {}
+
 module "vpc" {
-source = "terraform-aws-modules/vpc/aws"
-version = "~> 4.0"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
 
-
-name = "flask-eks-vpc"
-cidr = var.vpc_cidr
-
-
-azs = slice(data.aws_availability_zones.available.names, 0, 3)
-public_subnets = var.public_subnets
-private_subnets = var.private_subnets
-
-
-enable_nat_gateway = true
+  name = "flask-eks-vpc"
+  cidr = var.vpc_cidr
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  enable_nat_gateway = true
 }
 
 
 module "eks" {
-source = "terraform-aws-modules/eks/aws"
-version = "~> 19.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.8"
 
+  cluster_name    = var.cluster_name
+  cluster_version = "1.28"
 
-cluster_name = var.cluster_name
-cluster_version = "1.26"
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-
-subnets = module.vpc.private_subnets
-
-
-node_groups = {
-default = {
-desired_capacity = var.node_group_desired_capacity
-min_capacity = var.node_group_min_size
-max_capacity = var.node_group_max_size
-
-
-instance_type = "t3.medium"
-key_name = null
-}
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = var.node_group_desired_capacity
+      min_size       = var.node_group_min_size
+      max_size       = var.node_group_max_size
+      instance_types = ["t3.medium"]
+    }
+  }
 }
 
 
-manage_aws_auth = true
-}
 
 
 resource "aws_ecr_repository" "app_repo" {
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,14 @@
 terraform {
-required_version = ">= 1.0"
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = ">= 4.0"
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.40.0"
+    }
+  }
 }
-}
+
+provider "aws" {
+  region = var.aws_region
 }
